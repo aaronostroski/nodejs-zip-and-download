6f1e9e46418a31a0496f4ba09f01f85059a7d599
aaronostroski <aaronostroski@gmail.com>
2019-04-12 00:40:32 -0300

    Adicionando Adm-zip module e zipando imagens.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,78 @@
+// JSON Module
 let db = require('./config/anexo_01')
+
+// NodeJS Module
 const express = require('express');
-let Path = require('path')
-let fs = require('fs');
-let axios = require('axios')
 const app = express();
+const Path = require('path')
+const fs = require('fs');
+const axios = require('axios')
+const AdmZip = require('adm-zip');
+const zip = new AdmZip();
 
 function image(){
 
+    let count = 1;  
+    
     db.images.forEach((img) =>{
+        
+        let path = Path.resolve(__dirname, 'assents/image', `imagem${count}.png`)
+        let createStream = fs.createWriteStream(path);
 
-        let path = Path.resolve(__dirname, 'image', `imagem${Math.floor(Math.random() * (1000 - 1))}.png`)
-        let writer = fs.createWriteStream(path)
+        count++;
   
             axios({
                 url: img,
                 method: 'GET',
                 responseType: 'stream'
             })
-            .then(res => res.data.pipe(writer))
+            .then(res => res.data.pipe(createStream))
             .catch((err)=>{
                 console.log(err);
             })
 
             return new Promise((resolve, reject) => {
-                writer.on('finish', resolve)
-                writer.on('error', reject)
+                createStream.on('finish', ()=>{
+                    resolve()
+                })
+                createStream.on('error', ()=>{
+                    reject()
+                })
             })
 
     })
           
 }
 
+function zip1(){
+
+    let pathToZip = './assents/zip/image.zip';
+    
+    zip.addLocalFolder('./assents/image');
+    
+    zip.writeZip(pathToZip, (error)=>{
+    
+        console.log(error)
+    });
+    
+    
+    }
+    
+
 image();
 
+zip1();
+
+
+/*fs.readdir("./img", (err, files)=>{
+    console.log(err);
+
+    for(let file of files){
+
+        fs.unlinkSync(`./img/${file}`)
+    }
+} ) */
+    
 
 const PORT = process.env.PORT || 3000;
 
